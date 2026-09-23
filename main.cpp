c526c811b1b7366d0c38c90603baf4440c1290b8
FrogeCatLCode <126259143+FrogeCatLCode@users.noreply.github.com>
2026-09-23 17:00:20 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <FastLED.h>
+
+#define LED_PIN     4
+#define NUM_LEDS    60
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(255);
+}
+
+void loop() {
+  fill_solid(leds, NUM_LEDS, CRGB::Red);
+  FastLED.show();
+  delay(1000);
+  fill_solid(leds, NUM_LEDS, CRGB::Green);
+  FastLED.show();
+  delay(1000);
+}
