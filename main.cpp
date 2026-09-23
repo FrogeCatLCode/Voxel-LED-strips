dc3ea533286d0662730060f082a328d8f57e30dd
FrogeCatLCode <126259143+FrogeCatLCode@users.noreply.github.com>
2026-09-23 13:16:16 +0100

    Add files via upload
    First LED test to build firmware files on wokwi vs code

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+#define LED1 12
+
+void setup() {
+  pinMode(LED1, OUTPUT);
+}
+
+void loop () {
+  digitalWrite(LED1, HIGH);
+  delay(1000);
+  digitalWrite(LED1, LOW);
+  delay(1000);
+}
